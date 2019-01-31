c661b8a0abdc8de129bd13fb8053834a4adecbe8
Taylor <UkrainianEagle@q.com>
2019-01-31 01:17:54 -0600

    Updated some files; most of HTML & CSS are done

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// TODO:
+
+// Grab IDs and prep them for changing thei HTML/text
+
+// Set up click handlers for the gems
+    // Give the gems a value for each (watch the demo in classRep; numbers to use are in there)
+        // Same for total
+
+// Make RNG for goal & crystals
+    // Display goal value in #goal-text
+    // Find way to add up total value from clicking crystals and display in #current-total
+
+
+
+// OPTIONAL: make the color palette more RuneScape-esque
