2ee084e57fea000326c2aee29f6439f55970a441
Taylor <UkrainianEagle@q.com>
2019-01-31 20:05:21 -0600

    HTML & CSS almost done, save for color edits; JS started

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,51 @@
-// TODO:
 
-// Grab IDs and prep them for changing thei HTML/text
+
+
+
+
+
+
+
+// Grab gem IDs and prep them for changing the HTML/text
+$(function(){
+    $("#sapphire-gem").on("click", function(){
+        console.log("sapphire");
+    });
+    $("#emerald-gem").on("click", function(){
+        console.log("emerald");
+    });
+    $("#ruby-gem").on("click", function(){
+        console.log("ruby");
+    });
+    $("#diamond-gem").on("click", function(){
+        console.log("diamond");
+    });
+    // gems' RNG between 1 & 12 inclusively
+
+});
+
+// total number RNG between 19 & 120 inclusively
+let goalNumber = Math.floor(Math.random()*102) + Math.round(Math.random());
+
+$("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Set up click handlers for the gems
     // Give the gems a value for each (watch the demo in classRep; numbers to use are in there)
@@ -10,6 +55,3 @@
     // Display goal value in #goal-text
     // Find way to add up total value from clicking crystals and display in #current-total
 
-
-
-// OPTIONAL: make the color palette more RuneScape-esque
