1e44ab9c9b418c00e36d0d51e9c992759a280197
Taylor <UkrainianEagle@q.com>
2019-02-01 00:22:28 -0600

    Product is finished, but new design choices may arise later

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,14 @@
 
-
-
-
-
-
-
-
-// Grab gem IDs and prep them for changing the HTML/text
 $(function () {
-    
     // Set-up `current-sum = 0`, `wins = 0`, and `losses = 0` on game start
     let sum = 0;  // "Placeholder 0s"
     let goalNumber = 0;
     let gamesWon = 0;
     let gamesLost = 0;
-    
+
     $("#current-sum").html(sum);
     $("#wins-text").html("Wins: " + gamesWon);
     $("#losses-text").html("Losses: " + gamesLost);
-
 
 
     // Declared here rather than in setGemValues() as global variables for reset() to also use
@@ -27,14 +17,15 @@ $(function () {
     let rubyValue = 0;
     let diamondValue = 0;
 
-    function setGoal(){
+
+    function setGoal() {
         // total number RNG between 19 & 120 inclusively
         goalNumber = Math.floor(Math.random() * 102) + 19;
         $("#goal-value").html(goalNumber);
     }
-
     // Call the function to set a value for the goal
     setGoal();
+
 
     function setGemValues() {
         // Gems' RNG between 1 & 12 inclusively
@@ -53,7 +44,6 @@ $(function () {
             sapphireValue = oddNum + 1;
         }
     }
-
     // Call the function to set values for the gems
     setGemValues();
 
@@ -63,78 +53,49 @@ $(function () {
         sum += sapphireValue;
         checkGame();
         $("#current-sum").html(sum);
-        console.log("sapphire: ", sapphireValue);
     });
 
     $("#emerald-gem").on("click", function (event) {
         $("#current-sum").html(sum += emeraldValue);
         checkGame()
         $("#current-sum").html(sum);
-        console.log("emerald: ", emeraldValue);
     });
 
     $("#ruby-gem").on("click", function (event) {
         $("#current-sum").html(sum += rubyValue);
         checkGame()
         $("#current-sum").html(sum);
-        console.log("ruby: ", rubyValue);
     });
 
     $("#diamond-gem").on("click", function (event) {
         $("#current-sum").html(sum += diamondValue);
         checkGame()
         $("#current-sum").html(sum);
-        console.log("diamond: ", diamondValue);
     });
 
 
-
-
-
-
-
-
-// Check to see if the game should end
-function checkGame(){
-    // Win condition
-    if (sum === goalNumber) {
-        gamesWon++;
-        $("#wins-text").html("Wins: " + gamesWon);
-        reset();
+    // Check to see if the game should end
+    function checkGame() {
+        // Win condition
+        if (sum === goalNumber) {
+            gamesWon++;
+            $("#wins-text").html("Wins: " + gamesWon);
+            reset();
+        }
+        // Loss condition
+        else if (sum > goalNumber) {
+            gamesLost++
+            $("#losses-text").html("Losses: " + gamesLost);
+            reset();
+        }
     }
-    // Loss condition
-    else if (sum > goalNumber) {
-        gamesLost++
-        $("#losses-text").html("Losses: " + gamesLost);
-        reset();
+
+    // Function to reset the game (called after a win or loss)
+    function reset() {
+        sum = 0;
+        $("#current-sum").html(sum);
+        setGoal();
+        setGemValues();
     }
-}
-
-
-
-
-function reset() {
-    sum = 0;
-    $("#current-sum").html(sum);
-    setGoal();
-    setGemValues();
-}
-
 
 });
-
-
-
-
-
-
-
-
-// Set up click handlers for the gems
-    // Give the gems a value for each (watch the demo in classRep; numbers to use are in there)
-        // Same for total
-
-// Make RNG for goal & crystals
-    // Display goal value in #goal-text
-    // Find way to add up total value from clicking crystals and display in #current-total
-
